051ebb04dcb4265ba17bb79d4b243cac5a1ac2d7
ankitCompunnel <ankit.yadav@compunneldigital.com>
2026-07-31 14:37:07 +0530

    updated callback

--- src/Controllers/InstagramCallbackController.ts
+++ src/Controllers/InstagramCallbackController.ts
@@ -5,7 +5,7 @@ const APP_ID = process.env.INSTAGRAM_APP_ID || "1015452860015692";
 const APP_SECRET = process.env.INSTAGRAM_APP_SECRET || "76a8b193787892f6bf2459abeb935d7b";
 
 const REDIRECT_URI =
-    "https://unprofane-fluxionally-annalise.ngrok-free.dev/auth/instagram/callback";
+    "https://lynkupapi.lynkup.co.in/auth/instagram/callback";
 
 interface InstagramTokenResponse {
     access_token: string;
